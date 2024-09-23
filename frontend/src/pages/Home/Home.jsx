0504cf9e5016107b68fb21d40f0ba9682bcc9477
edsonaraujoneto <edsonaraujodesouzaneto@gmail.com>
2024-09-23 14:04:50 -0300

    feat: add last users at screen home

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,19 +1,43 @@
-import "./Home.css"
-import { Header } from "../../components/Header"
-import image from "../../assets/image.jpg"
+import "./Home.css";
+import { Header } from "../../components/Header";
+import image from "../../assets/image.jpg";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-export function Home () {
+export function Home() {
+    const [users, setUsers] = useState([]);
+
+    const fetchUsers = async () => {
+        try {
+            const response = await axios.get("http://localhost:3030/user/getAllUsers");
+            setUsers(response.data); 
+        } catch (error) {
+            console.error("Erro ao buscar usuários:", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
 
     return (
         <div className="body">
-            <Header/>
+            <Header />
             <main className="main">
                 <div className="main-div">
                     <h2 className="title">Enboost</h2>
                     <p>O site <strong>friend</strong> para o seu aprendizado de inglês</p>
                 </div>
-                <img src={image} alt="Pesquise" className="image"/>
+                <img src={image} alt="Pesquise" className="image" />
             </main>
+            <footer className="footer">
+                <h3>Usuários que já usaram o Enboost:</h3>
+                <ul>
+                    {users.map((user) => (
+                        <li key={user.id}>{user.full_name}</li>
+                    ))}
+                </ul>
+            </footer>
         </div>
-    )
+    );
 }
